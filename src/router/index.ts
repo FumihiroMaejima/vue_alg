c1088d878f5fbc44ee18f093383278529d82fe0b
Fumihiro_Maejima <testerwatson1101@gmail.com>
2020-09-05 01:44:14 +0900

    make test2 page.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Arg from '../views/Arg.vue'
 import Test1 from '../views/Test1.vue'
+import Test2 from '../views/Test2.vue'
 // import Home from '../views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
@@ -13,6 +14,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/test1',
     name: 'Test1',
     component: Test1
+  },
+  {
+    path: '/test2',
+    name: 'Test2',
+    component: Test2
   }
 ]
 
---
+++ src/views/Test2.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="bg-blue-500 lg:bg-green-500">
+    <header class="container mx-auto text-white">
+      <div class="flex justify-between items-center">
+        <h1 class="text-4xl font-semibold md:text-xl">Header</h1>
+        <div class="md:hidden">
+          <button>
+            <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+              <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div>
+        <ul class="md:flex md:justify-center">
+          <li class="border-b md:border-none">
+            <a href="#" class="block px-8 py-2 hover:bg-blue-600 rounded">
+              test項目1
+            </a>
+          </li>
+          <li class="border-b md:border-none">
+            <a href="#" class="block px-8 py-2 hover:bg-blue-600 rounded">
+              test項目2
+            </a>
+          </li>
+          <li class="border-b md:border-none">
+            <a href="#" class="block px-8 py-2 hover:bg-blue-600 rounded">
+              test項目3
+            </a>
+          </li>
+          <li class="border-b md:border-none">
+            <a href="#" class="block px-8 py-2 hover:bg-blue-600 rounded">
+              test項目4
+            </a>
+          </li>
+          <li class="border-b md:border-none">
+            <div class="ml-4 my-2">
+              <a
+                href="#"
+                class="px-4 py-2 bg-orange-500 hover:bg-orange-400 rounded-full"
+              >
+                test項目5
+              </a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+  components: {
+    // HelloWorld
+  }
+})
+export default class Test2 extends Vue {}
+</script>
